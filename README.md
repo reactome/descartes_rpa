5c83d02be28fed45f53bbbb4a31669d7f3500308
Meirelles_JL <jldemeirelles@gmail.com>
2021-06-09 18:59:04 -0300

    Added long description and updated README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# descartes
+# descartes-rpa
+
+This project aims to create a dockerized python pipeline to extract pathway activity from single-cell clusters in a systematic manner, annotating each cluster with which pathway it represents.
+
+## Installing
+
+```bash
+conda env create -f env.yml
+```
+
+Activate the environment
+```bash
+conda activate descartes-rpa
+```
--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
 setup(
     name="descartes_rpa",
     version="1.0",
     description="descartes-rpa: Extract pathway features from Single-Cell",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/reactome/descartes-rpa",
     python_requires=">=3.9",
     author="Joao Luiz de Meirelles",
