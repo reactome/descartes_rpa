9d2e5f7a672e6d2c08cca24e20b1fe80019f7731
Meirelles_JL <jldemeirelles@gmail.com>
2021-06-09 15:30:19 -0300

    Changing docstring of function

--- descartes_rpa/fetch/descartes.py
+++ descartes_rpa/fetch/descartes.py
@@ -2,20 +2,21 @@ import requests
 
 
 def fetch_descartes_tissue(tissue: str, out_file: str) -> None:
-    """Function to fetch Raw Gene Count Sparse Matrices from Single-Cell
+    """Function to fetch Loom Single-Cell tissue data from
     Descartes human database.
 
     Args:
         tissue: Name of the tissue that is going to be downloaded
-        out_file: Output file that is going to store .RDS data
+        out_file: Output file that is going to store .loom data
 
     Examples:
-        >>> fetch_descartes_tissue("Thymus", "Thymus.RDS")
+        >>> fetch_descartes_tissue("Thymus", "Thymus.loom")
 
     """
     url = (
         "https://shendure-web.gs.washington.edu/content/members/cao1025/"
-        "public/FCA_RNA_supp_files/scanpy_cells_by_tissue/{}_processed.loom"
+        "public/FCA_RNA_supp_files/scanpy_cells_by_tissue/"
+        f"{tissue}_processed.loom"
     )
     data = requests.get(url, allow_redirects=True)
     with open(out_file, 'wb') as out:
