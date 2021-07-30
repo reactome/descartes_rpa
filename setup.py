c1998a92f35081b3ed0f939b9b06e27512d78c66
jldemeirelles <joao.meirelles@neoprospecta.com>
2021-07-30 20:33:50 -0300

    Release version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="descartes_rpa",
-    version="1.0",
+    version="1.1",
     description="descartes_rpa: Extract pathway features from Single-Cell",
     long_description=long_description,
     long_description_content_type='text/markdown',
